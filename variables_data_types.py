9e49250b7e45cd1720ea6aabe0155710189f540c
smileyharini424-cloud <smileyharini424@gmail.com>
2026-09-18 07:36:06 -0700

    Create variables_data_types.py

---
+++ variables_data_types.py
@@ -0,0 +1,22 @@
+age = 20
+height = 5.6
+name = "Harini"
+student = True
+
+print("Age:", age)
+print("Type:", type(age))
+
+print()
+
+print("Height:", height)
+print("Type:", type(height))
+
+print()
+
+print("Name:", name)
+print("Type:", type(name))
+
+print()
+
+print("Student:", student)
+print("Type:", type(student))
